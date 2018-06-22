24c9e1224498f483c1e2746d19520777dfa93fa9
Franco Catena <francocatena@gmail.com>
2018-06-22 17:04:26 -0300

    Bamboo 1.0.0

--- lib/ist_web/router.ex
+++ lib/ist_web/router.ex
@@ -17,7 +17,7 @@ defmodule IstWeb.Router do
   end
 
   if Mix.env() == :dev do
-    forward("/sent_emails", Bamboo.EmailPreviewPlug)
+    forward("/sent_emails", Bamboo.SentEmailViewerPlug)
   end
 
   scope "/", IstWeb do
