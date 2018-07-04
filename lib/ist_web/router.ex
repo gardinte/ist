1b197da958417c3eae079f51907f734023071ce8
Franco Catena <francocatena@gmail.com>
2018-07-04 12:52:41 -0300

    CSP headers added

--- lib/ist_web/router.ex
+++ lib/ist_web/router.ex
@@ -1,13 +1,21 @@
 defmodule IstWeb.Router do
   use IstWeb, :router
 
+  @csp [
+    "default-src 'self'",
+    "script-src 'self'",
+    "style-src 'self' 'unsafe-inline'"
+  ]
+
+  @csp_headers %{"content-security-policy" => Enum.join(@csp, ";")}
+
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
     plug(:fetch_flash)
     plug(:fetch_current_session)
     plug(:protect_from_forgery)
-    plug(:put_secure_browser_headers)
+    plug(:put_secure_browser_headers, @csp_headers)
     plug(:put_cache_control_headers)
     plug(:put_breadcrumb, name: "≡", url: "/")
   end
