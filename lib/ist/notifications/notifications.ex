b6e2dfab5251bd51b19d32d0efd640d04ce9c626
Franco Catena <francocatena@gmail.com>
2019-08-06 16:04:50 -0300

    Change send_password_reset code style

--- lib/ist/notifications/notifications.ex
+++ lib/ist/notifications/notifications.ex
@@ -3,7 +3,8 @@ defmodule Ist.Notifications do
   alias Ist.Notifications.{Mailer, Email}
 
   def send_password_reset(%User{} = user) do
-    Email.password_reset(user)
+    user
+    |> Email.password_reset()
     |> Mailer.deliver_later()
   end
 end
