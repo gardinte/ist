d64f4cb523c903e9b12715f63e8230a982944536
Franco Catena <francocatena@gmail.com>
2019-08-18 00:05:35 -0300

    Elixir 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ist.Mixfile do
     [
       app: :ist,
       version: "0.0.1",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
