6a721cae1274c9a503eb5f51413487cf4eb09eac
Franco Catena <francocatena@gmail.com>
2019-10-18 16:55:13 -0300

    Add bamboo local adapter to dev config

--- config/dev.exs
+++ config/dev.exs
@@ -43,6 +43,9 @@ config :ist, IstWeb.Endpoint,
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+# Bamboo local adapter
+config :ist, Ist.Notifications.Mailer, adapter: Bamboo.LocalAdapter
+
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
