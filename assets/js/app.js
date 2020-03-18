959f8cb4753c8a62fb7b127e6eaa8f5a26d5d670
Franco Catena <francocatena@gmail.com>
2020-03-18 18:04:24 -0300

    Setup _ready_ to use videojs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,6 @@
 import 'phoenix_html'
+import 'video.js'
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
 import './font_awesome'
 import './stimulus'
 import './turbolinks'
-import videojs from 'video.js'
-
-window.videojs = videojs
-
-import '@videojs/http-streaming'
